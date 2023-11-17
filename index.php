4e092b0044228b14dbacca6c09f3b3a4f74c8db0
Joshua Beyond <37176065+joshuabeyondfin@users.noreply.github.com>
2023-11-18 00:42:29 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * 
+ * Main Template file
+ * 
+ * @package jpwr
+ * 
+ */
+
+ ?>
+ 
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>WordPress theme</title>
+ </head>
+ <body>
+  Hello WordPress
+ </body>
+ </html>
